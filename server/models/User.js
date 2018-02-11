aba595df4efb155eafe6e3faac0b29b2345af4a3
The Editor <me@theeditorstudio.com>
2018-02-11 17:44:07 -0500

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose').set('debug', true);
+const mongoose = require('mongoose');
 const db = mongoose.connect(require('../config').database);
 
 var schema = mongoose.Schema({
